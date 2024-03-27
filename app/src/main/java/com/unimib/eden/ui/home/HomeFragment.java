f76794b2ec243b1c363d231c75c340b53648071f
GaviraghiElia <74615025+GaviraghiElia@users.noreply.github.com>
2024-03-27 17:11:11 +0100

    Register complete and login ok

--- app/src/main/java/com/unimib/eden/ui/home/HomeFragment.java
+++ app/src/main/java/com/unimib/eden/ui/home/HomeFragment.java
@@ -8,6 +8,8 @@ import androidx.annotation.NonNull;
 import androidx.annotation.RequiresApi;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.navigation.NavController;
+import androidx.navigation.fragment.NavHostFragment;
 
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -16,6 +18,7 @@ import android.view.ViewGroup;
 
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
@@ -34,9 +37,10 @@ public class HomeFragment extends Fragment {
 
     private static final String TAG = "HomeFragment";
     private FragmentHomeBinding binding;
+    private FirebaseAuth mAuth;
     FirebaseFirestore db = FirebaseFirestore.getInstance();
     private List<Pianta> piante = new ArrayList<Pianta>();
-
+    private NavController navController;
     private HomeViewModel homeViewModel;
 
     public HomeFragment() {
@@ -49,10 +53,10 @@ public class HomeFragment extends Fragment {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setHasOptionsMenu(true);
-
         homeViewModel = new ViewModelProvider(this).get(HomeViewModel.class);
         Log.d(TAG, "onCreate: " + homeViewModel.getPiante());
-
+        mAuth = FirebaseAuth.getInstance();
+        Log.d("mAuth", "mAuth success");
 
     }
 
@@ -62,8 +66,23 @@ public class HomeFragment extends Fragment {
         binding = FragmentHomeBinding.inflate(inflater, container, false);
         View view = binding.getRoot();
         homeViewModel.updateDB();
+        navController = NavHostFragment.findNavController(this);
         // Inflate the layout for this fragment
         return view;
 
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        if(currentUser != null)
+        {
+            Log.d("mAuth", "current user != null");
+        }else
+        {
+            Log.d("mAuth", "current user == null");
+            navController.navigate(R.id.action_navigation_home_to_loginFragment);
+        }
     }
 }
